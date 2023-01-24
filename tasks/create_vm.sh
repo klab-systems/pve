bba3a54bdcba0e16baaffa6455853f962ea68a05
Kevin Crull <kevin.crull@perforce.com>
2023-01-24 15:32:11 -0500

    working on create_vm currently in progress

--- tasks/create_vm.sh
+++ tasks/create_vm.sh
@@ -4,6 +4,11 @@ vmid to clone
 new vmid
 description
 vm name
-storage
-
+storage (default local-lvm)
+storage increase size (starts ~2GB) qm resize vmid scsi0 +10G
+increase mem (starts at 2048) qm set vmid --sshkey <sshkey>
+increase cpu (starts at 1)
+username qm set vmid --ciuser <username>
+password qm set vmid --cipassword <password>
+ipconfig = qm set vmid --ipconfig0 ip=<ip>,gw=<gw>
 
