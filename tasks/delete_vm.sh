4ff7bd8a56ef8a172245783955c8b0ea864c9b61
Kevin Crull <kevin.crull@perforce.com>
2023-01-24 12:49:52 -0500

    adding exit codes to delete_vm

--- tasks/delete_vm.sh
+++ tasks/delete_vm.sh
@@ -5,6 +5,8 @@
 vmidcheck=$(qm list | grep "$PT_vmid")
 if [ -n "$vmidcheck" ]; then
     qm destroy $PT_vmid --destroy-unreferenced-disks --purge --skiplock
+    exit 0
 else
     echo "VMID not found"
+    exit 1
 fi
